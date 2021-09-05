6de3b7ccc14f564d1fb399b07de48b14e79ee3e0
ZoeyTheEgoist <jragon.alt1@gmail.com>
2021-09-05 21:06:47 +0800

    Update DispenserBehaviorMixin.java

--- LenientStackSize/src/main/java/com/astrazoey/lenientstacksize/mixins/DispenserBehaviorMixin.java
+++ LenientStackSize/src/main/java/com/astrazoey/lenientstacksize/mixins/DispenserBehaviorMixin.java
@@ -2,39 +2,36 @@ package com.astrazoey.lenientstacksize.mixins;
 
 import com.astrazoey.lenientstacksize.DispenserBehaviorInterface;
 import net.minecraft.block.DispenserBlock;
-import net.minecraft.block.dispenser.DispenserBehavior;
 import net.minecraft.block.dispenser.ItemDispenserBehavior;
 import net.minecraft.block.entity.DispenserBlockEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.FluidModificationItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPointer;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(targets = "net/minecraft/block/dispenser/DispenserBehavior$8")
-public class DispenserBehaviorMixin implements DispenserBehaviorInterface {
-    private ItemDispenserBehavior fallbackBehavior = new ItemDispenserBehavior();
+public class DispenserBehaviorMixin {
 
-
-    @Override
+    /**
+     * @author Astrazoey
+     */
+    @Overwrite
     public ItemStack dispenseSilently(BlockPointer pointer, ItemStack stack) {
+
+        ItemDispenserBehavior fallbackBehavior = new ItemDispenserBehavior();
 
         ItemStack emptyBucketStack = new ItemStack(Items.BUCKET);
 
         FluidModificationItem fluidModificationItem = (FluidModificationItem) stack.getItem();
-        BlockPos blockPos = pointer.getPos().offset((Direction) pointer.getBlockState().get(DispenserBlock.FACING));
+        BlockPos blockPos = pointer.getPos().offset(pointer.getBlockState().get(DispenserBlock.FACING));
         World world = pointer.getWorld();
-        if (fluidModificationItem.placeFluid((PlayerEntity) null, world, blockPos, (BlockHitResult) null)) {
-            fluidModificationItem.onEmptied((PlayerEntity) null, world, stack, blockPos);
+        if (fluidModificationItem.placeFluid( null, world, blockPos, null)) {
+            fluidModificationItem.onEmptied( null, world, stack, blockPos);
 
             if (stack.getCount() > 1) {
                 System.out.println("Found stack! Get count = " + stack.getCount());
@@ -42,7 +39,7 @@ public class DispenserBehaviorMixin implements DispenserBehaviorInterface {
                 newStack.decrement(1);
 
                 if (((DispenserBlockEntity)pointer.getBlockEntity()).addToFirstFreeSlot(emptyBucketStack.copy()) < 0) {
-                    this.fallbackBehavior.dispense(pointer, emptyBucketStack.copy());
+                    fallbackBehavior.dispense(pointer, emptyBucketStack.copy());
                 }
 
                 return newStack;
@@ -53,7 +50,7 @@ public class DispenserBehaviorMixin implements DispenserBehaviorInterface {
 
         } else {
             System.out.println("Fallback behavior activated!");
-            return this.fallbackBehavior.dispense(pointer, stack);
+            return fallbackBehavior.dispense(pointer, stack);
         }
     }
 }
